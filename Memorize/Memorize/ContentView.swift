668e62bc24ef19f88613150c1c908058a89d2c4a
Azat <8280770+azataiot@users.noreply.github.com>
2022-09-02 23:30:45 +0300

    l2 almost finished

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -14,11 +14,14 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            HStack{
-                ForEach(emojis[0..<emojiCount ],id: \.self, content: { emoji in
-                    CardView(content: emoji)
-                })
+            ScrollView{
+                LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]){
+                    ForEach(emojis[0..<emojiCount ],id: \.self, content: { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    })
+                }
             }
+            .foregroundColor(.red)
             Spacer()
             HStack{
                 remove
@@ -29,7 +32,7 @@ struct ContentView: View {
             .font(.largeTitle)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+        
     }
     
     var remove:some View {
@@ -66,7 +69,7 @@ struct CardView : View {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp{
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             } else {
                 shape.fill()
