dcf026ff6d940d2e56f4c8fa424595ab4d780b2f
Azat <8280770+azataiot@users.noreply.github.com>
2022-09-05 23:22:51 +0300

    assignment ok

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,122 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["🚗","🚕","🚙","🚌","🚎","🏎","🚓","🚑","🚒","🚐","🛻","🚚","🚛","🚜","🦯","🦽","🦼","🩼","🛴","🚲","🛵","🏍","🛺","🛞"]
-    
-    @State var emojiCount = 6
-    
+    static let emojisCar = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🦯", "🦽", "🦼", "🩼", "🛴", "🚲", "🛵", "🏍", "🛺", "🛞"]
+
+    static let emojiFood = ["🌭", "🌮", "🌯", "🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🫐"]
+
+    static let emojiHeart = ["❤️", "🧡", "💛", "💚", "💙", "💜", "🖤", "🤍", "🤎", "💔", "❤️‍🔥", "❤️‍🩹", "❣️"]
+
+    // Task 5 card number is a variable in range 7..<12
+    @State var emojiCount = 12
+    @State var emojis = emojisCar
+    @State var themeColor: Color = .red
+
+
     var body: some View {
-        VStack{
-            ScrollView{
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 64))]){
-                    ForEach(emojis[0..<emojiCount ],id: \.self, content: { emoji in
-                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+
+        VStack {
+            HStack {
+                // Task 3. Add title Memorize!
+                Text("Memorize!")
+                        .font(.largeTitle)
+            }
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 64))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in
+                        CardView(content: emoji).aspectRatio(2 / 3, contentMode: .fit)
                     })
                 }
             }
-            .foregroundColor(.red)
+                    .foregroundColor(themeColor)
             Spacer()
-            HStack{
-                remove
+            HStack {
+                // Task 4. Theme choosing buttons
+                Button(action: {
+                    // Task 6. array content is shuffled.
+                    emojis = ContentView.emojisCar.shuffled()
+                    themeColor = .red
+                    emojiCount = Int.random(in: 7..<12)
+                }, label: {
+                    VStack {
+                        Image(systemName: "car").font(.largeTitle)
+                        Text("Vehicles")
+                    }
+                })
+                        .foregroundColor(.red)
                 Spacer()
-                add
+                // Task 7 & 8. Button including an image with SF Symbol and Text label
+                Button(action: {
+                    emojis = ContentView.emojiFood.shuffled()
+                    themeColor = .orange
+                    emojiCount = Int.random(in: 7..<12)
+                }, label: {
+                    VStack {
+                        Image(systemName: "fork.knife").font(.largeTitle)
+                        Text("Food")
+                    }
+                })
+                        .foregroundColor(.orange)
+                Spacer()
+                Button(action: {
+                    emojis = ContentView.emojiHeart.shuffled()
+                    themeColor = .green
+                    emojiCount = Int.random(in: 7..<12)
+                }, label: {
+                    VStack {
+                        Image(systemName: "heart").font(.largeTitle)
+                        Text("Heart")
+                    }
+                })
+                        .foregroundColor(.green)
             }
-            .padding(.horizontal)
-            .font(.largeTitle)
+                    .padding(.horizontal)
+                    // Task 9 using smaller font
+                    .font(.title3)
+
         }
-        .padding(.horizontal)
-        
+                .padding()
     }
-    
-    var remove:some View {
-        Button(action: {
-            if emojiCount > 1 {
-                emojiCount -= 1
+
+    // UI works in both portrait and landscape mode
+
+    struct CardView: View {
+        var content: String
+        @State var isFaceUp: Bool = true
+
+        var body: some View {
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 20)
+                if isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: 3)
+                    Text(content).font(.largeTitle)
+                } else {
+                    shape.fill()
+                }
             }
-        }, label: {
-            Image(systemName: "minus.circle")
-        })
-    }
-    
-    var add:some View {
-        Button(action: {
-            if emojiCount < emojis.count {
-                emojiCount += 1
-            }
-        }, label: {
-            Image(systemName: "plus.circle")
-        })
-    }
-}
-
-
-
-
-
-struct CardView : View {
-    var content:String
-    @State var isFaceUp:Bool = true
-    
-    var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if isFaceUp{
-                shape.fill().foregroundColor(.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
-            } else {
-                shape.fill()
-            }
+                    .onTapGesture {
+                        isFaceUp = !isFaceUp
+                    }
         }
-        .onTapGesture {
-            isFaceUp = !isFaceUp
-        }
+
     }
-    
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .previewInterfaceOrientation(.landscapeLeft)
-        ContentView()
-            .preferredColorScheme(.dark)
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                    .previewInterfaceOrientation(.portrait)
+            ContentView()
+                    .preferredColorScheme(.dark)
+        }
     }
 }
